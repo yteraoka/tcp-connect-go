76a6f8b5c5fccbc0013f31fb7d7275f47f48bcf0
Yoshinori Teraoka <teraoka@3-shake.com>
2020-04-14 19:41:41 +0900

    TLS support

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ https://tech.xing.com/a-reason-for-unexplained-connection-timeouts-on-kubernetes
 https://docs.aws.amazon.com/eks/latest/userguide/external-snat.html
 
 ```
-Usage: ./tcp-connect-go [-timeout N] [-count M] [-parallel X] [-threshold Y] [-verbose] server:port
+Usage: ./tcp-connect-go [-timeout N] [-count M] [-parallel X] [-threshold Y] [-verbose] [-tls] [-insecure] [-servername www.example.com] server:port
 ```
 
 ```
@@ -15,12 +15,20 @@ Usage: ./tcp-connect-go [-timeout N] [-count M] [-parallel X] [-threshold Y] [-v
 Usage of ./tcp-connect-go:
   -count int
     	Number of connect (default 1)
+  -insecure
+    	Do not verify certificate
   -parallel int
     	Number of go routines (default 1)
+  -servername string
+    	SNI
   -threshold int
     	Duration time threshold in millisecond. Show duration time if over this (default 200)
   -timeout int
     	Connect timeout in second (default 10)
+  -tls
+    	Enable TLS mode
   -verbose
     	Enable verbose mode (show duration time every time)
+  -version
+    	Show version and exit
 ```
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 	"sync"
 	"time"
 )
@@ -18,6 +20,8 @@ var (
 
 var thresholdMs int64
 var verbose bool
+var useTls bool
+var tlsConfig = &tls.Config{}
 
 func times(f func(string, time.Duration), count int, remote string, timeout time.Duration, wg *sync.WaitGroup) {
 	defer wg.Done()
@@ -29,7 +33,13 @@ func times(f func(string, time.Duration), count int, remote string, timeout time
 func connect(remote string, timeout time.Duration) {
 	d := net.Dialer{Timeout: timeout}
 	start := time.Now()
-	conn, err := d.Dial("tcp4", remote)
+	var conn net.Conn
+	var err error
+	if useTls {
+		conn, err = tls.DialWithDialer(&d, "tcp", remote, tlsConfig)
+	} else {
+		conn, err = d.Dial("tcp4", remote)
+	}
 	if err != nil {
 		fmt.Printf("Can't connect to %s: %v\n", remote, err)
 		return
@@ -46,8 +56,11 @@ func main() {
 	var timeout = flag.Int("timeout", 10, "Connect timeout in second")
 	var count = flag.Int("count", 1, "Number of connect")
 	var parallel = flag.Int("parallel", 1, "Number of go routines")
+	var serverName = flag.String("servername", "", "SNI")
 	flag.Int64Var(&thresholdMs, "threshold", 200, "Duration time threshold in millisecond. Show duration time if over this")
 	flag.BoolVar(&verbose, "verbose", false, "Enable verbose mode (show duration time every time)")
+	flag.BoolVar(&useTls, "tls", false, "Enable TLS mode")
+	var insecure = flag.Bool("insecure", false, "Do not verify certificate")
 	var versionFlag = flag.Bool("version", false, "Show version and exit")
 
 	flag.Parse()
@@ -58,11 +71,18 @@ func main() {
 	}
 
 	if flag.NArg() != 1 {
-		fmt.Printf("Usage: %s [-timeout N] [-count M] [-parallel X] [-threshold Y] [-verbose] server:port\n", os.Args[0])
+		fmt.Printf("Usage: %s [-timeout N] [-count M] [-parallel X] [-threshold Y] [-verbose] [-tls] [-insecure] [-servername www.example.com] server:port\n", os.Args[0])
 		os.Exit(1)
 	}
 
 	remoteHostPort := flag.Args()[0]
+
+	if useTls {
+		if *serverName == "" {
+			*serverName = strings.Split(remoteHostPort, ":")[0]
+		}
+		tlsConfig = &tls.Config{ServerName: *serverName, InsecureSkipVerify: *insecure}
+	}
 
 	var wg sync.WaitGroup
 
